04c7195f79f540edd3046c35fcd9536ba26b414c
Gaetano Salvatore Falco <56557552+chowned@users.noreply.github.com>
2022-10-10 10:40:03 +0200

    Update Ex1.sql

--- DWH_Test/Ex_In_Class/Ex1.sql
+++ DWH_Test/Ex_In_Class/Ex1.sql
@@ -20,18 +20,18 @@ CREATE TABLE SalesEX1(
     amount int
 );
 
-INSERT INTO `SalesEX1`(city,dated,amount) VALUES('TURIN','10-10-22',100);
-INSERT INTO `SalesEX1`(city,dated,amount) VALUES('TURIN','11-10-22',200);
-INSERT INTO `SalesEX1`(city,dated,amount) VALUES('TURIN','12-10-22',300);
-INSERT INTO `SalesEX1`(city,dated,amount) VALUES('TURIN','13-10-22',400);
-INSERT INTO `SalesEX1`(city,dated,amount) VALUES('MILAN','10-10-22',120);
-INSERT INTO `SalesEX1`(city,dated,amount) VALUES('MILAN','11-10-22',230);
-INSERT INTO `SalesEX1`(city,dated,amount) VALUES('MILAN','12-10-22',340);
-INSERT INTO `SalesEX1`(city,dated,amount) VALUES('MILAN','14-10-22',460);
-INSERT INTO `SalesEX1`(city,dated,amount) VALUES('ROME','10-10-22',120);
-INSERT INTO `SalesEX1`(city,dated,amount) VALUES('ROME','11-10-22',240);
-INSERT INTO `SalesEX1`(city,dated,amount) VALUES('ROME','12-10-22',350);
-INSERT INTO `SalesEX1`(city,dated,amount) VALUES('ROME','14-10-22',470);
+INSERT INTO `SalesEX1`(city,dated,amount) VALUES('TURIN','22-10-10',100);
+INSERT INTO `SalesEX1`(city,dated,amount) VALUES('TURIN','22-10-11',200);
+INSERT INTO `SalesEX1`(city,dated,amount) VALUES('TURIN','22-10-12',300);
+INSERT INTO `SalesEX1`(city,dated,amount) VALUES('TURIN','22-10-13',400);
+INSERT INTO `SalesEX1`(city,dated,amount) VALUES('MILAN','22-10-10',120);
+INSERT INTO `SalesEX1`(city,dated,amount) VALUES('MILAN','22-10-11',230);
+INSERT INTO `SalesEX1`(city,dated,amount) VALUES('MILAN','22-10-12',340);
+INSERT INTO `SalesEX1`(city,dated,amount) VALUES('MILAN','22-10-14',460);
+INSERT INTO `SalesEX1`(city,dated,amount) VALUES('ROME','22-10-10',120);
+INSERT INTO `SalesEX1`(city,dated,amount) VALUES('ROME','22-10-11',240);
+INSERT INTO `SalesEX1`(city,dated,amount) VALUES('ROME','22-10-12',350);
+INSERT INTO `SalesEX1`(city,dated,amount) VALUES('ROME','22-10-14',470);
 
 select city, 
 dated,
